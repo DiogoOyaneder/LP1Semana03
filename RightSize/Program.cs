78ba579e08e2ee8fcd8becb3fb95295f0b2c773e
DiogoOyaneder <diogo.oyaneder@gmail.com>
2025-03-07 09:07:24 +0000

    add RightSize

---
+++ RightSize/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace RightSize
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            foreach (string arg in args)
+            {
+                if (arg.Length >= 8)
+                {
+                    Console.WriteLine("[EARLY STOP]");
+                    break;
+
+
+                }
+
+            }
+        }
+    }
+}
