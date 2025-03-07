51bbc22c07c0e634c5bb6267a76a96cdfb689e98
DiogoOyaneder <diogo.oyaneder@gmail.com>
2025-03-07 09:31:44 +0000

    add foreach loop and output

--- ChangeString/Program.cs
+++ ChangeString/Program.cs
@@ -4,9 +4,28 @@ namespace ChangeString
 {
     public class Program
     {
-        private static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            Console.Write("String: ");
+            string s = Console.ReadLine(); 
+            Console.Write("Caráter: ");
+            char character = Console.ReadLine()[0];
+            string final = ("");
+            Console.Write ("Será impresso: ");
+            
+            foreach (char c in s)
+            {
+                    if (c == character)
+                {
+                    final += "x";
+                }
+                    else
+                {
+                   final += c;
+                }
+            }
+
+            Console.WriteLine(final);
         }
     }
 }
